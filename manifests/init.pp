fd8d2f333cc67ae6d9a3986213bdcedac69448be
Micah Anderson <micah@riseup.net>
2009-10-02 16:16:44 -0400

    move variables into the :client define

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,9 +12,7 @@
 # 1. include stunnel: this will automatically include stunnel::debian,
 #    which automatically includes stunnel::linux, which automatically
 #    includes stunnel::base
-# 2. stunnel::config allows you to configure the general stunnel configuration
-#    e.g. stunnel::config { configdir => '/etc/stunnel4', enable => false }
-# 3. stunnel::client allows you to configure different /etc/stunnel/*.conf files
+# 2. stunnel::client allows you to configure different /etc/stunnel/*.conf files
 #    to provide various stunnel configurations
 
 # TODO: warn on cert/key issues, fail on false accept?
@@ -26,18 +24,27 @@ class stunnel {
     default: { include stunnel::default }
   }
 
-  define client ( $ensure = present, $accept = false, $CApath = false, $CAfile = false,
-                  $cert = false, $ciphers = false, $client = false, $connect = false,
-                  $CRLpath = false, $CRLfile = false, $delay = false, $engineNum = false,
-                  $exec = false, $execargs = false, $failover = false, $ident = false,
-                  $key = false, $local = false, $OSCP = false, $OCSPflag = false,
-                  $options = false, $pid = false, $protocol = false,
-                  $protocolAuthentication = false, $protocolHost = false,
-                  $protocolPassword = false, $protocolUsername = false, $pty = false,
-                  $retry = false, $session = false, $sslVersion = "SSLv3",
-                  $stack = false, $TIMEOUTbusy = false, $TIMEOUTclose = false,
-                  $TIMEOUTconnect = false, $TIMEOUTidle = false, $transparent = false,
-                  $verify = false ) {
+  define client ( $ensure = present, $accept = false, $CApath = false,
+                  $CAfile = false, $cert = false, $chroot = false,
+                  $ciphers = false, $client = false, $compress =
+                  false, $connect = false, $CRLpath = false, $CRLfile
+                  = false, $debuglevel = false, $delay = false, $EGD =
+                  false, $engine = false, $engineCtrl = false,
+                  $engineNum = false, $exec = false, $execargs =
+                  false, $failover = false, $ident = false, $key =
+                  false, $local = false, $OSCP = false, $OCSPflag =
+                  false, $options = false, $output = false, $pid =
+                  false, $protocol = false, $protocolAuthentication =
+                  false, $protocolHost = false, $protocolPassword =
+                  false, $protocolUsername = false, $pty = false,
+                  $retry = false, $RNDbytes = false, $RNDfile = false,
+                  $RNDoverwrite = false, $service = false, $session =
+                  false, $setuid = "stunnel4", $setgid = "stunnel4",
+                  $socket = [ "l:TCP_NODELAY=1, "r:TCP_NODELAY=1"],
+                  $sslVersion = "SSLv3", $stack = false, $syslog =
+                  false, $TIMEOUTbusy = false, $TIMEOUTclose = false,
+                  $TIMEOUTconnect = false, $TIMEOUTidle = false,
+                  $transparent = false, $verify = false ) {
 
     $real_client = $client ? { default => "yes" }
     $real_pid = $pid ? { false => "/${name}.pid", default => $pid }
@@ -52,85 +59,7 @@ class stunnel {
   }
 }
 
-class stunnel::config {
-  
-  $chroot = $chroot_override ? {
-    '' => "/var/lib/stunnel4",
-    default => $chroot_override
-  }
-  
-  $compression = $compression_override ? {
-    '' => false,
-    default => $compression_override
-  }
-  
-  $debuglevel = $debuglevel_override ? {
-    '' => false,
-    default => $debuglevel_override
-  }
-  
-  $EGD = $EGD_override ? {
-    '' => false,
-    default => $EGD_override
-  }
-  
-  $engine = $engine_override ? {
-    '' => false,
-    default => $engine_override
-  }
-  
-  $engineCtrl = $engineCtrl_override ? {
-    '' => false,
-    default => $engineCtrl_override
-  }
-  
-  $output = $output_override ? {
-    '' => false,
-    default => $output_override
-  }
-  
-  $RNDbytes = $RNDbytes_override ? {
-    '' => false,
-    default => $RNDbytes_override
-  }
-  
-  $RNDfile = $RNDfile_override ? {
-    '' => false,
-    default => $RNDfile_override
-  }
-  
-  $RNDoverwrite = $RNDoverwrite_override ? {
-    '' => false,
-    default => $RNDoverwrite_override
-  }
-  
-  $service = $service_override ? {
-    '' => false,
-    default => $service_override
-  }
-  
-  $setuid = $setuid_override ? {
-    '' => "stunnel4",
-    default => $setuid_override
-  }
-  
-  $setgid = $setgid_override ? {
-    '' => "stunnel4",
-    default => $setgid_override
-  }
-  
-  $socket = $socket_override ? {
-    '' => [ "l:TCP_NODELAY=1", "r:TCP_NODELAY=1" ],
-    default => $socket_override
-  }
-  
-  $syslog = $syslog_override ? {
-    '' => false,
-    default => $syslog_override
-  }
-}
-
-class stunnel::base inherits stunnel::config {
+class stunnel::base {
   
   case $stunnel_ensure_version {
     '': { $stunnel_ensure_version = "present" }
